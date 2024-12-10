ae0ab186b7b941c35bf726889275f07d1fbfc7c2
Leonardo Silveira <sombriks@gmail.com>
2024-12-10 16:34:51 -0300

    reversing deck

--- src/bld/java/io/sombriks/TictacticstoeBuild.java
+++ src/bld/java/io/sombriks/TictacticstoeBuild.java
@@ -24,10 +24,7 @@ public class TictacticstoeBuild extends Project {
                 .include(dependency("commons-io:commons-io:2.16.1"))
                 .include(dependency("org.slf4j:slf4j-simple:2.0.16"))
                 .include(dependency("de.neuland-bfi:pug4j:2.2.0"))
-                .include(dependency("io.javalin:javalin:6.3.0"));
-
-        //
-        scope(runtime)
+                .include(dependency("io.javalin:javalin:6.3.0"))
                 .include(dependency("org.webjars.npm:alpinejs:3.14.3"))
                 .include(dependency("org.webjars.npm:htmx.org:2.0.2"));
 
