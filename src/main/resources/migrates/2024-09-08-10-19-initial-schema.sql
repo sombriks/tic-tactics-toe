360947b2e38b4fb424ce3f4f75ae9f54bb8353e2
Leonardo Silveira <sombriks@gmail.com>
2024-12-12 18:56:36 -0300

    carga de dados de teste

--- src/main/resources/migrates/2024-09-08-10-19-initial-schema.sql
+++ src/main/resources/migrates/2024-09-08-10-19-initial-schema.sql
@@ -1,100 +1,103 @@
 -- all boards dwell in a world map
 create table if not exists maps
 (
-    id   identity primary key,
-    name varchar(255) unique not null,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    id      identity primary key,
+    name    varchar(255) unique not null,
+    created timestamp           not null default now(),
+    updated timestamp                    default now()
 );
 
 -- players may exist even without a map
 create table if not exists players
 (
-    id identity primary key,
-    uid varchar(255) unique not null,
-    handle varchar(255) unique not null,
-    email varchar(255) unique,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    id      identity primary key,
+    uid     varchar(255) unique not null,
+    handle  varchar(255) unique not null,
+    email   varchar(255) unique,
+    created timestamp           not null default now(),
+    updated timestamp                    default now()
+);
+
+create table if not exists card_types
+(
+    id           identity primary key,
+    title        varchar(255) not null,
+    description  text         not null,
+    combat_power integer      not null default 1,
+    -- TODO side effects: reveal, on attack, on efend, etc
+    created      timestamp    not null default now(),
+    updated      timestamp             default now()
+);
+
+create table if not exists cards
+(
+    id                   identity primary key,
+    current_combat_power integer   not null default 1,
+    players_id           integer   not null references players (id) on delete cascade,
+    card_types_id        integer   not null references card_types (id) on delete cascade,
+    created              timestamp not null default now(),
+    updated              timestamp          default now()
+);
+
+create table if not exists decks
+(
+    id          identity primary key,
+    description text      not null,
+    players_id  integer   not null references players (id) on delete cascade,
+    cards_id    integer   not null references cards (id) on delete cascade,
+    created     timestamp not null default now(),
+    updated     timestamp          default now()
 );
 
 create table if not exists boards
 (
     id         identity primary key,
-    maps_id    integer not null references maps (id) on delete cascade,
-    players_id integer not null references players (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    maps_id    integer   not null references maps (id) on delete cascade,
+    players_id integer   not null references players (id) on delete cascade,
+    created    timestamp not null default now(),
+    updated    timestamp          default now()
 );
 
 create table if not exists challenges
 (
-    id        identity primary key,
-    boards_id integer not null references boards (id) on delete cascade,
+    id         identity primary key,
+    boards_id  integer   not null references boards (id) on delete cascade,
     -- the attacker id
-    players_id integer not null references players (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    players_id integer   not null references players (id) on delete cascade,
+    created    timestamp not null default now(),
+    updated    timestamp          default now()
 );
 
 create table if not exists moves
 (
-    id        identity primary key,
-    challenges_id integer not null references challenges (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
-);
-
-create table if not exists decks
-(
-    id         identity primary key,
-    players_id integer not null references players (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    id            identity primary key,
+    challenges_id integer   not null references challenges (id) on delete cascade,
+    created       timestamp not null default now(),
+    updated       timestamp          default now()
 );
 
 create table if not exists settings
 (
     id         identity primary key,
-    players_id integer not null references players (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
-);
-
-create table if not exists card_types
-(
-    id identity primary key,
-    title varchar (255) not null,
-    description text not null,
-    combat_power integer not null default 1,
-    -- TODO side effects: reveal, on attack, etc
-    created timestamp not null default now(),
-    updated timestamp default now()
-);
-
-create table if not exists cards
-(
-    id            identity primary key,
-    players_id    integer not null references players (id) on delete cascade,
-    card_types_id integer not null references card_types (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now()
+    players_id integer   not null references players (id) on delete cascade,
+    created    timestamp not null default now(),
+    updated    timestamp          default now()
 );
 
 create table if not exists decks_cards
 (
-    decks_id integer not null references decks (id) on delete cascade,
-    cards_id integer not null references cards (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now(),
+    decks_id integer   not null references decks (id) on delete cascade,
+    cards_id integer   not null references cards (id) on delete cascade,
+    created  timestamp not null default now(),
+    updated  timestamp          default now(),
     primary key (decks_id, cards_id)
 );
 
 create table if not exists boards_cards
 (
-    boards_id integer not null references boards (id) on delete cascade,
-    cards_id integer not null references cards (id) on delete cascade,
-    created timestamp not null default now(),
-    updated timestamp default now(),
+    boards_id integer   not null references boards (id) on delete cascade,
+    cards_id  integer   not null references cards (id) on delete cascade,
+    created   timestamp not null default now(),
+    updated   timestamp          default now(),
     primary key (boards_id, cards_id)
 );
--- src/main/resources/migrates/2024-09-10-12-01-initial-data.sql
+++ src/main/resources/migrates/2024-09-10-12-01-initial-data.sql
@@ -1,11 +1,31 @@
 -- some support basic data
 
-insert into maps (name) values ('The great one');
+insert into maps (name)
+values ('The great one');
 
-insert into players (uid, handle) values ('aaaa', 'player 1');
-insert into players (uid, handle) values ('bbbb', 'player 2');
+insert into players (uid, handle)
+values ('aaaa', 'player 1'),
+       ('bbbb', 'player 2');
 
-insert into boards(maps_id, players_id) values (1,1);
+insert into card_types (title, description, combat_power)
+values ('the bush', 'simple, harmless bush sitting in the field', 1),
+       ('peasant', 'humble worker trying to earn a life', 1),
+       ('stray dog', 'has the wits to survive', 2),
+       ('rolling stone', 'no one stays in the path of this rock', 3),
+       ('soldier', 'guy who serves in the army', 5),
+       ('catapult', 'throw thing at unadvised people', 8),
+       ('the tower', 'watch tower to opress them all', 13),
+       ('wizard', 'spells that can ruin with your day', 21),
+       ('dragon', 'fire', 34),
+       ('mystic knight', 'that OP character', 55),
+       ('ancient evil', 'unspeakable evil', 89),
+       ('bob', 'hi bob', 144);
 
-insert into challenges(boards_id, players_id) values (1,2);
+
+
+insert into boards(maps_id, players_id)
+values (1, 1);
+
+insert into challenges(boards_id, players_id)
+values (1, 2);
 
