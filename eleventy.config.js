d118e7d80cc46affe0dd7ee9716eb1bc914a51c5
Leonardo Silveira <sombriks@gmail.com>
2025-01-25 19:33:26 -0300

    add state

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,7 +14,5 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "src/assets": "/",
     "node_modules/alpinejs/dist/cdn.js": "/alpine.js",
-    "node_modules/the-monospace-web-alt/reset.css": "/reset.css",
-    "node_modules/the-monospace-web-alt/index.css": "/tmw.css",
   })
 }
