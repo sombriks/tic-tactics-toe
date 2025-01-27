60aad10bbe6f8c5b88e750403d4d08774f33489c
Leonardo Silveira <sombriks@gmail.com>
2025-01-27 15:38:47 -0300

    improving

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,23 +1,5 @@
 // tic tactics toe basic cards
 
-const warrior = {a: 5, d: 4, name: "warrior"}
-const wizard = {a: 2, d: 7, name: "wizard"}
-const archer = {a: 3, d: 6, name: "archer"}
-const barbarian = {a: 7, d: 2, name: "barbarian"}
-const druid = {a: 4, d: 5, name: "druid"}
-const rogue = {a: 2, d: 7, name: "rogue"}
-const paladin = {a: 8, d: 1, name: "paladin"}
-const necromancer = {a: 2, d: 7, mame: "necromancer"}
-const monk = {a: 1, d: 8, name: "monk"}
-
-const deck = [warrior, warrior, paladin, //
-  paladin, paladin, monk,//
-  archer, rogue, barbarian]
-
-const board = [archer, wizard, monk, //
-  archer, wizard, monk, //
-  archer, wizard, monk]
-
 const challenge = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 const moves = []
@@ -48,11 +30,12 @@ function fight({cardFromBoard, cardFromDeck}) {
   // battle! TODO implement effects (reveal, attack, defend) and terrains
   const ch = cardFromBoard.dataset.index
   challenge[ch] = cardFromDeck.dataset.attack - cardFromBoard.dataset.defense
-  moves.push({cardFromBoard, cardFromDeck})
   if(challenge[ch] > 0) {
     cardFromBoard.classList.remove("ready")
     cardFromBoard.classList.add("hide")
   }
+  moves.push({cardFromBoard, cardFromDeck})
+  console.log(`${cardFromDeck.dataset.code} attacks ${cardFromBoard.dataset.code}`)
 }
 
 function printBoard() {
